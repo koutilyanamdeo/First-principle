13fa5762e62aceff407d5938138c3a156c9795bb
koutilyanamdeo <70022358+koutilyanamdeo@users.noreply.github.com>
2021-11-21 16:30:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header';
+import Image from './src/images/author-1.jpg';
+
+function App() {
+  return (
+    <div>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
